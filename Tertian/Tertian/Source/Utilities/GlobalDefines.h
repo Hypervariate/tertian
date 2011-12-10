4974e76033d49c52bae18b92a04f9848deb72775
Manuel A. Rodriguez <rodriguezm@gmail.com>
2011-12-10 17:54:09 -0500

    updated window creation

--- Tertian/Tertian/Source/Utilities/GlobalDefines.h
+++ Tertian/Tertian/Source/Utilities/GlobalDefines.h
@@ -1,9 +1,12 @@
 #ifndef GLOBALDEFINES_H_
 #define GLOBALDEFINES_H_
 
-
+#define WINDOW_TITLE "Tertian v0.000000000000001"
 #define WINDOW_WIDTH 1024
 #define WINDOW_HEIGHT 768
+
+
+static SDL_Window* g_SDLwindow = 0;
 
 
 #endif
